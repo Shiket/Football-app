a4ebba231834f05f48f4d052b971663ab96d65d7
Shiket <shiket95@gmail.com>
2019-11-05 17:35:00 +0100

    Updates

---
+++ precache-manifest.f8ce85f979adda37ec03d15d18868ca8.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "03db5b0b2e3c5686b5708e19c64cc738",
+    "url": "/Football-app/index.html"
+  },
+  {
+    "revision": "e9773c385e73251bdf7a",
+    "url": "/Football-app/static/css/2.11f871ab.chunk.css"
+  },
+  {
+    "revision": "e9773c385e73251bdf7a",
+    "url": "/Football-app/static/js/2.d829479d.chunk.js"
+  },
+  {
+    "revision": "5ad8fe5bfa34c3e508e5",
+    "url": "/Football-app/static/js/main.01903047.chunk.js"
+  },
+  {
+    "revision": "978f1778318314a13007",
+    "url": "/Football-app/static/js/runtime-main.415055f1.js"
+  },
+  {
+    "revision": "5961a9b26e449d5a3250beb99982f716",
+    "url": "/Football-app/static/media/bg1.5961a9b2.jpg"
+  },
+  {
+    "revision": "b6f79b7af6bc1ada72f25f3fe430f919",
+    "url": "/Football-app/static/media/calendar.b6f79b7a.png"
+  },
+  {
+    "revision": "9073818b18607d2ca7948d97d3b6a932",
+    "url": "/Football-app/static/media/calendar2.9073818b.png"
+  },
+  {
+    "revision": "837ca175c86f963659c78fe86863d99c",
+    "url": "/Football-app/static/media/fb.837ca175.png"
+  },
+  {
+    "revision": "83bdb11073c5715436a6e51d2824236f",
+    "url": "/Football-app/static/media/ig.83bdb110.png"
+  },
+  {
+    "revision": "62ddbbd091730900d67a731eebb4778f",
+    "url": "/Football-app/static/media/playerIcon.62ddbbd0.png"
+  },
+  {
+    "revision": "b54e7511d9748bcda60a53871760637b",
+    "url": "/Football-app/static/media/twitter.b54e7511.png"
+  },
+  {
+    "revision": "a338def485c78aadc66645d7727580be",
+    "url": "/Football-app/static/media/website.a338def4.png"
+  },
+  {
+    "revision": "6e6734ff1c8857be530277b165ab32c4",
+    "url": "/Football-app/static/media/yt.6e6734ff.png"
+  }
+]);
